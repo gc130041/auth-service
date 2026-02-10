be0fd0766e674ed46b52de7018a6de4c6d02aa77
gc130041 <ocontreras-2024150@kinal.edu.gt>
2026-02-10 09:26:51 -0600

    Agregar Cambios en clase

---
+++ src/AuthService.Domain/Entities/Role.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthService.Domain.Entities;
+
+public class Role
+{
+    [key]
+    [MaxLength(16)]
+    public string Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+
+    [Required]
+    [MaxLength(255)]
+    public string Description { get; set; }
+
+    //Relaciones con UserRole
+    public ICollection<UserRole> UserRoles { get; set; }
+
+}
+
+/*
+Roles
++--------------+--------------+------------------+
+| Id           | Name         | Description      |
++--------------+--------------+------------------+
+| ADMIN        | Admin        | Administrador    |
+| USER         | User         | Usuario normal   |
+| GUEST        | Guest        | Invitado         |
++--------------+--------------+------------------+
+*/
---
+++ src/AuthService.Domain/Entities/User.cs
@@ -0,0 +1,49 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace AuthService.Domain.Entities;
+
+public class User
+{
+    [Key]
+    [MaxLength(16)]
+    public string Id { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "El nombre es obligatorio")]
+    [MaxLength(25)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "El apellido es obligatorio")]
+    [MaxLength(25)]
+    public string Surname { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(50)]
+    public string Username { get; set; } = string.Empty;
+
+    [Required]
+    [EmailAdress] //El valor de esta propiedad debe tener un formato de correo electronico
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(255)]
+    public string Password { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(25)]
+    public bool Status { get; set; } = true;
+
+    [Required]
+    public DateTime CreatedAt { get; set; }
+
+    [Required]
+    public DateTime UpdatedAt { get; set; }
+
+    //Relaciones de navegación solo dentro del código
+    //Esto no altera la base de datos
+    public UserProfile Profile { get; set;} = null!;
+    public ICollection<UserRole> UserRoles { get; set; } = [];
+    public UserEmail UserEmail { get; set; } = null!;
+    public UserPasswordReset PasswordReset { get; set; } = null!;
+
+}
+
